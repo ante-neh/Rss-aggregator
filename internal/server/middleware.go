431882319e79b9a7df610a500ea577dcf0c329b2
ante-neh <amareanteneh12@gmail.com>
2024-08-16 01:27:35 +0300

    update(backend):add secureHeader middleware

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -40,3 +40,12 @@ func(s *Server) authMiddleware(next authHandler) http.HandlerFunc{
 		next(w, r, user)
 	}
 }
+
+
+func(s *Server) secureHeaders(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+        w.Header().Set("X-Frame-Options", "deny")
+		next.ServeHTTP(w, r)
+	})
+}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,12 +2,12 @@ package server
 
 import "net/http"
 
-func (s *Server) Router() *http.ServeMux{
+func (s *Server) Router() http.Handler{
 	mux := http.NewServeMux()
 	mux.Handle("GET /api/v1/healthz", http.HandlerFunc(s.handleHealthz))
 	mux.Handle("POST /api/v1/users", http.HandlerFunc(s.handleUserCreate))
 	mux.Handle("GET /api/v1/users/", s.authMiddleware((s.handleGetUser)))
 
 	
-	return mux 
+	return s.secureHeaders(mux) 
 }
